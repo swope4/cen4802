423baf8172340904987f8574c523c4ee38883eab
swope4 <78827851+swope4@users.noreply.github.com>
2022-03-06 23:40:53 -0500

    Junit Testing of Gen1
    word generator tested with j unit.

---
+++ Gen1Test.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Gen1Test {
+
+	@Test
+	void testGen1() {
+		fail("Not yet implemented");
+		}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
